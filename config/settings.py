026e83bf3261707d204be31fff101cc14057f6d1
Daniel <buh.add@gmail.com>
2017-08-15 18:30:13 +0000

    Set default permission classes to DjangoObjectPermissions

--- config/settings.py
+++ config/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+        'rest_framework.permissions.DjangoObjectPermissions',
     ],
     'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': (
